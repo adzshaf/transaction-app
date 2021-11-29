a6d5f4f2c56039387eb6593a1a18f10f6bcff35a
Shafiya Adzhani <adz.arsym@gmail.com>
2021-11-30 02:32:49 +0800

    add sign in screen
    Signed-off-by: Shafiya Adzhani <adz.arsym@gmail.com>

---
+++ src/components/SignInScreen.js
@@ -0,0 +1,64 @@
+import * as React from 'react';
+import {View, StyleSheet} from 'react-native';
+import {useTheme} from 'react-native-paper';
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from '@react-native-google-signin/google-signin';
+import {CLIENT_ID} from '@env';
+
+const SignInScreen = () => {
+  const {colors} = useTheme();
+  const styles = makeStyles(colors);
+
+  const signIn = async () => {
+    GoogleSignin.configure({
+      webClientId: CLIENT_ID,
+      offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+      profileImageSize: 120, // [iOS] The desired height (and width) of the profile image. Defaults to 120px
+    });
+    console.log(CLIENT_ID);
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      const accessToken = userInfo.idToken;
+      const user = userInfo.user;
+      console.log('userinfo', userInfo);
+    } catch (error) {
+      console.log(error);
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        // operation (e.g. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        // play services not available or outdated
+      } else {
+        // some other error happened
+      }
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <GoogleSigninButton
+        style={{width: 240, height: 80}}
+        size={GoogleSigninButton.Size.Wide}
+        color={GoogleSigninButton.Color.Dark}
+        onPress={() => signIn()}
+      />
+    </View>
+  );
+};
+
+const makeStyles = colors =>
+  StyleSheet.create({
+    container: {
+      flex: 1,
+      padding: 15,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+  });
+
+export default SignInScreen;
